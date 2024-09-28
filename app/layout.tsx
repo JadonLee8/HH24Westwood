17f875e429487fde97e14d2699c24f55c6dd3f08
Alex Bui <zanbowie138@gmail.com>
2024-09-28 10:28:15 -0500

    nextjs starter

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './globals.css'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+
+export default function RootLayout({ children }: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <head />
+            <body className="flex flex-col min-h-screen relative">
+                <Header />
+                <div className="px-2 py-5">
+                    {children}
+                </div>
+                <Footer />
+            </body>
+        </html>
+    )
+}
