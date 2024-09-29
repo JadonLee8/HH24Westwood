e7eae5dce4bf362854214e636359e6875409ef35
Jadon Lee <jadonlee888@gmail.com>
2024-09-29 00:41:02 -0500

    use game context to store the crime prompt

--- frontend/app/game/components/GameFrame.tsx
+++ frontend/app/game/components/GameFrame.tsx
@@ -6,14 +6,13 @@ import { useState } from 'react';
 
 export default function GameFrame() {
     const game = useGameContext();
-    const [input, setInput] = useState<string>("");
 
     return (
         <div className="flex items-center justify-center min-h-screen shadow-2xl">
             <div className="bg-amber-900 p-5 m-5 rounded-md">
                 <h1 className="text-5xl text-white font-western">Enter your crime:</h1>
                 <h1 className="text-3xl text-white font-western1">1 to 5 sentences in first person</h1>
-                <PlayerInput onChange={setInput} />
+                <PlayerInput onChange={game.setCrimePrompt} />
                 <div className="flex">
                     <button className="py-2 px-4 
                     bg-red-600 border-black  rounded-md w-1/4 font-western2 shadow-md hover:shadow-lg" onClick={() => {}}>Submit</button>
